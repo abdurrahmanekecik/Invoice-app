49b2acbb95f7e0af1e66cd7150bfefcce6b10ce3
abdurrahmanekecik <abdurrahmanekecik@gmail.com>
2024-02-05 14:56:13 +0300

    Invoice Item Create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
             CounterSeeder::class,
             ProductSeeder::class,
             CustomerSeeder::class,
+            InvoiceSeeder::class,
+            InvoiceItemSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/InvoiceItemSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\InvoiceItem;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class InvoiceItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        InvoiceItem::factory()->count(10)->create();
+    }
+}
