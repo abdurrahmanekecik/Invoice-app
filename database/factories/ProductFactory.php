d80115b3cb8ea657b551eb08e741d3c5e520e2b3
abdurrahmanekecik <abdurrahmanekecik@gmail.com>
2024-02-05 13:28:12 +0300

    Products Area Added

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,11 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'item_code' => 'IC-1000'.rand(10, 500),
+            'name' => 'Name of Product'.rand(10, 500),
+            'description' => 'Description of Product'.rand(10, 500),
+            'unit_price' => mt_rand(100, 1000),
+
         ];
     }
 }
